70aba9e7e38d2a5ffef1642808680003fb180854
Derek Reynolds <derekreynoldsis@gmail.com>
2021-06-26 22:38:02 +0100

    Updates

---
+++ precache-manifest.8e07bce4debabe29e3a4e492cf447853.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e1f622d83adc7c10a6a580d77864c6a",
+    "url": "/ireland-in-graphs/index.html"
+  },
+  {
+    "revision": "3fff45b148152f405c4f",
+    "url": "/ireland-in-graphs/static/css/main.3266f6e7.chunk.css"
+  },
+  {
+    "revision": "3c2190d95be80fb5c9ac",
+    "url": "/ireland-in-graphs/static/js/2.2a60a082.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/ireland-in-graphs/static/js/2.2a60a082.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fff45b148152f405c4f",
+    "url": "/ireland-in-graphs/static/js/main.c1455745.chunk.js"
+  },
+  {
+    "revision": "148587dd121061349a67",
+    "url": "/ireland-in-graphs/static/js/runtime-main.83ab00e2.js"
+  },
+  {
+    "revision": "1f2f383076b2af7cd06689ab15fd8a2e",
+    "url": "/ireland-in-graphs/static/media/belmullet.1f2f3830.csv"
+  },
+  {
+    "revision": "5e8894d3390adc414e1b7016dbe01c62",
+    "url": "/ireland-in-graphs/static/media/casement.5e8894d3.csv"
+  },
+  {
+    "revision": "6eccc713c5cae60f358b205290b812e1",
+    "url": "/ireland-in-graphs/static/media/cork.6eccc713.csv"
+  },
+  {
+    "revision": "9946e7487ff3c6255a86c7f57714d645",
+    "url": "/ireland-in-graphs/static/media/dublin.9946e748.csv"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/ireland-in-graphs/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "6364522e6c14071e88a26f1e33aafaab",
+    "url": "/ireland-in-graphs/static/media/malinhead.6364522e.csv"
+  },
+  {
+    "revision": "bf22b006c46a23f2305752590418b76f",
+    "url": "/ireland-in-graphs/static/media/mullingar.bf22b006.csv"
+  },
+  {
+    "revision": "87652951df3fba56966b07efafbf4c91",
+    "url": "/ireland-in-graphs/static/media/shannon.87652951.csv"
+  },
+  {
+    "revision": "d25b375d75926bd7c2baa98cd6a45b26",
+    "url": "/ireland-in-graphs/static/media/valencia.d25b375d.csv"
+  }
+]);
